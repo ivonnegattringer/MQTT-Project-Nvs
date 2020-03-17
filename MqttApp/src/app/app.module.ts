7641f2e7354f4feb126306eb6a794fafb1d7f7ed
ivonnegattringer <ivonne.gattringer@gmail.com>
2020-03-17 16:52:16 +0100

    created my own theme

--- MqttApp/src/app/app.module.ts
+++ MqttApp/src/app/app.module.ts
@@ -5,6 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import {MatButtonModule} from '@angular/material/button'; 
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 
 @NgModule({
   declarations: [
@@ -12,8 +17,13 @@ import { environment } from '../environments/environment';
   ],
   imports: [
     BrowserModule,
+    MatSlideToggleModule,
+    MatTabsModule,
+    MatSliderModule,
+    MatButtonModule,
     AppRoutingModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
